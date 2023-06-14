6e1d9872619d6fc5e6aee4b1acc471d4396f33ea
ForhadRoben <i.rforhad@gmail.com>
2023-06-15 04:15:55 +0600

    navbar and footer added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,4 +1,6 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../pages/Shared/NavBar/NavBar";
+import Footer from "../pages/Shared/Footer/Footer";
 // import Footer from "../pages/Shared/Footer/Footer";
 // import NavBar from "../pages/Shared/NavBar/NavBar";
 
@@ -6,13 +8,13 @@ import { Outlet } from "react-router-dom";
 const Main = () => {
     return (
         <div>
-            {/* <NavBar></NavBar> */}
+            <NavBar></NavBar>
 
             <div className="max-w-screen-xl mx-auto mb-8">
                 <Outlet></Outlet>
             </div>
 
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,39 @@
+
+import { Link } from 'react-router-dom';
+import logo1 from '../../../assets/logo1.jpg'
+
+const NavBar = () => {
+
+    const navbar =
+        <>
+            <li><Link to='/'>Home</Link ></li>
+            <li><Link to='/alltoys'>All Toys</Link></li>
+            <li><Link to='/blogs'>Blogs</Link></li>
+
+        </>
+    return (
+        <div>
+            <div className="navbar bg-cyan-300 font-bold text-white">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-cyan-300 z-10 rounded-box w-52">
+                            {navbar}
+                        </ul>
+                    </div>
+                    <a className=" inline-flex w-12 h-12 text-4xl"><img className='rounded-full' src={logo1} alt="website_logo" />WeKiddies</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navbar}
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
