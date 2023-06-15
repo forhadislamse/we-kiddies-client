284ca29cf35fc5a1dd8429ed40f419dbd3151521
ForhadRoben <i.rforhad@gmail.com>
2023-06-15 07:23:12 +0600

    error page  added

---
+++ src/pages/Shared/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,37 @@
+import { FaHome } from "react-icons/fa";
+import { Link, useRouteError, } from 'react-router-dom'
+import errorImg from '../../../assets/404.jpg'
+import { Helmet } from "react-helmet-async";
+
+
+const ErrorPage = () => {
+    const { error, status } = useRouteError();
+
+
+    return (
+        <div className="grid grid-cols-1 w-full">
+            <Helmet>
+                <title>
+                    WeKiddies | Not Found
+                </title>
+            </Helmet>
+            <div>
+                <img className="mx-auto" src={errorImg} alt="" />
+            </div>
+            <div className="lg:mt-20 text-center">
+                <p className="text-2xl md:text-4xl text-red-500">Opps!!! Page not found.</p>
+                <h2 className='mb-8 font-extrabold text-9xl text-red-500'>
+                    <span className='sr-only'>Error</span> {status || 404}
+                </h2>
+                <p className='text-2xl font-semibold md:text-3xl mb-8'>
+                    {error?.message}
+                </p>
+                <Link to="/" className="btn btn-error"><FaHome className="text-3xl"></FaHome> Go To Home</Link>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default ErrorPage;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,11 +3,12 @@ import {
 } from "react-router-dom";
 import Home from "../pages/Home/Home/Home";
 import Main from "../layout/Main";
+import ErrorPage from "../pages/Shared/ErrorPage/ErrorPage";
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Main></Main>,
-        // errorElement: <ErrorPage></ErrorPage>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
